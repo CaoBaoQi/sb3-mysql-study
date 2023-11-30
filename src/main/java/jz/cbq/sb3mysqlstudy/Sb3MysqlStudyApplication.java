7df284bee893473998c2097c6e132e12e751efa3
曹宝琪 <2023cbq@gmail.com>
2023-11-30 18:59:42 +0800

    init commit

---
+++ src/main/java/jz/cbq/sb3mysqlstudy/Sb3MysqlStudyApplication.java
@@ -0,0 +1,13 @@
+package jz.cbq.sb3mysqlstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3MysqlStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb3MysqlStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jz/cbq/sb3mysqlstudy/Sb3MysqlStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jz.cbq.sb3mysqlstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3MysqlStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
